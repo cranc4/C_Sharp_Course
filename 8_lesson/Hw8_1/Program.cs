66c3442484dc8e7c2ccfe2d18ebd1ba06e529033
Artem_S <cranc4568@gmail.com>
2022-12-17 00:07:37 +0300

    Hw8_1

---
+++ 8_lesson/Hw8_1/Program.cs
@@ -0,0 +1,71 @@
+﻿// 1: Задайте двумерный массив. Напишите программу,
+//  которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+void Sort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp1 = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp1;
+                }
+                for (int l = 0; l < array.GetLength(0) - 1; l++)
+                {
+                    if (array[l, j] < array[l + 1, j])
+                    {
+                        int temp2 = array[l + 1, j];
+                        array[l + 1, j] = array[l, j];
+                        array[l, j] = temp2;
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+
+Print(arr_1);
+Sort(arr_1);
+Console.WriteLine();
+Print(arr_1);
